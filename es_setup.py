20084faa21e23249c5cc7a661fd83fd3ca4fa51c
Isaacpm <andrago.suscripcions@gmail.com>
2016-04-09 20:24:38 +0100

    added elasticsearch setup script
    The script creates the templates for the 4 data indexes and their
    aliases

---
+++ es_setup.py
@@ -0,0 +1,25 @@
+"""
+Script to create the required dede elasticsearch index templates
+The templates set the creation of a .raw not analyzed field for each string field with less than 256 characters and an alias for each main data index.
+The aliases will be used to access the data from a single point of query, as the different services create their own indices.
+"""
+from elasticsearch import Elasticsearch
+
+#Connect to local ES cluster
+es = Elasticsearch(request_timeout=60)
+
+#Index templates configuration
+job_data_template = {"order": 0,"template": "dede_job_data*", "settings": {}, "mappings": { "_default_": { "dynamic_templates": [{ "strings": { "mapping": { "type": "string", "fields": { "raw": { "index": "not_analyzed", "ignore_above": 256, "type": "string"}}},"match_mapping_type": "string"}}]}},"aliases": {"dede_job_data": {}}}
+
+job_summary_template = {"order": 0, "template": "dede_job_summary*", "settings": {}, "mappings": {"_default_": {"dynamic_templates": [{"strings": {"mapping": {"type": "string", "fields": {"raw": {"index": "not_analyzed", "ignore_above": 256, "type": "string"}}}, "match_mapping_type": "string"}}]}}, "aliases": {"dede_job_summary": {}}}
+
+
+job_tracking_template = {"order": 0,"template": "dede_job_tracking*","settings": {},"mappings": {"_default_": {"dynamic_templates": [{"strings": {"mapping": {"type": "string", "fields": {"raw": {"index": "not_analyzed", "ignore_above": 256, "type": "string"}}}, "match_mapping_type": "string"}}]}}, "aliases": {"dede_job_tracking": {}}}
+
+job_matrix_template = {"order": 0,"template": "dede_job_matrix*","settings": {},"mappings": {"_default_": {"dynamic_templates": [{"strings": {"mapping": {"type": "string", "fields": {"raw": {"index": "not_analyzed","ignore_above": 256,"type": "string"}}}, "match_mapping_type": "string"}}]}}, "aliases": {"dede_job_matrix": {}}}
+
+#Create templates
+es.indices.put_template(name="dede_job_data", body=job_data_template)
+es.indices.put_template(name="dede_job_summary", body=job_summary_template)
+es.indices.put_template(name="dede_job_data", body=job_tracking_template)
+es.indices.put_template(name="dede_job_data", body=job_matrix_template)

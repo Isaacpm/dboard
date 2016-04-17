e2411a3f2c40b54d1d2ab11000d7e001cb20ed6d
Isaacpm <andrago.suscripcions@gmail.com>
2016-04-17 22:28:06 +0100

    Added dashboard and visualizations
    Added initial dashboard and visualizations

--- kibana_setup.py
+++ kibana_setup.py
@@ -40,3 +40,73 @@ job_data_pattern = {
 es.index(index=".kibana", doc_type="index-pattern", id="job_matrix",body=job_matrix)
 es.index(index=".kibana", doc_type="index-pattern", id="job_tracking",body=job_data_tracking_pattern)
 es.index(index=".kibana", doc_type="index-pattern", id="job_data",body=job_data_pattern)
+
+#Prepare visualizations bodies
+
+services_parameters = {
+      "title": "services_parameters",
+      "visState": "{\"title\":\"services_parameters\",\"type\":\"table\",\"params\":{\"perPage\":5,\"showPartialRows\":false,\"showMeticsAtAllLevels\":false},\"aggs\":[{\"id\":\"1\",\"type\":\"cardinality\",\"schema\":\"metric\",\"params\":{\"field\":\"service_name.raw\",\"customLabel\":\"Count\"}},{\"id\":\"2\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"service_name.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"Service Name\"}},{\"id\":\"3\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"solver_type.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"Solver\"}},{\"id\":\"4\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"activation.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"Activation\"}},{\"id\":\"5\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"template.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"Template\"}},{\"id\":\"6\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"base_lr\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"LR\"}},{\"id\":\"7\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"layers\",\"size\":5,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"Layers\"}},{\"id\":\"8\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"batch_size\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\",\"customLabel\":\"Batch Size\"}}],\"listeners\":{}}",
+      "uiStateJSON": "{}",
+      "description": "",
+      "version": 1,
+      "kibanaSavedObjectMeta": {
+        "searchSourceJSON": "{\"index\":\"dede_job_data\",\"query\":{\"query_string\":{\"analyze_wildcard\":true,\"query\":\"*\"}},\"filter\":[]}"
+      }
+  }
+
+job_data_leaderboard = {
+          "title": "job_data_leaderboard",
+          "visState": "{\"title\":\"job_data_leaderboard\",\"type\":\"table\",\"params\":{\"perPage\":5,\"showMeticsAtAllLevels\":false,\"showPartialRows\":false},\"aggs\":[{\"id\":\"2\",\"type\":\"max\",\"schema\":\"metric\",\"params\":{\"field\":\"precision\",\"customLabel\":\"Max Precision\"}},{\"id\":\"3\",\"type\":\"terms\",\"schema\":\"bucket\",\"params\":{\"field\":\"service_name.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"2\",\"customLabel\":\"Service Name\"}},{\"id\":\"5\",\"type\":\"max\",\"schema\":\"metric\",\"params\":{\"field\":\"accp\",\"customLabel\":\"Max ACCP\"}},{\"id\":\"7\",\"type\":\"max\",\"schema\":\"metric\",\"params\":{\"field\":\"f1\",\"customLabel\":\"Max F1\"}},{\"id\":\"9\",\"type\":\"max\",\"schema\":\"metric\",\"params\":{\"field\":\"recall\",\"customLabel\":\"Max Recall\"}}],\"listeners\":{}}",
+          "uiStateJSON": "{}",
+          "description": "",
+          "version": 1,
+          "kibanaSavedObjectMeta": {
+            "searchSourceJSON": "{\"index\":\"dede_job_data\",\"query\":{\"query_string\":{\"analyze_wildcard\":true,\"query\":\"*\"}},\"filter\":[]}"
+          }
+          }
+
+accp_by_service_name = {
+          "title": "accp_by_service_name",
+          "visState": "{\"title\":\"precision_by_service_name\",\"type\":\"line\",\"params\":{\"addLegend\":true,\"addTimeMarker\":false,\"addTooltip\":true,\"defaultYExtents\":false,\"drawLinesBetweenPoints\":true,\"interpolate\":\"linear\",\"radiusRatio\":9,\"scale\":\"linear\",\"setYExtents\":false,\"shareYAxis\":true,\"showCircles\":true,\"smoothLines\":false,\"times\":[],\"yAxis\":{}},\"aggs\":[{\"id\":\"1\",\"type\":\"max\",\"schema\":\"metric\",\"params\":{\"field\":\"accp\"}},{\"id\":\"2\",\"type\":\"histogram\",\"schema\":\"segment\",\"params\":{\"field\":\"iteration\",\"interval\":200,\"extended_bounds\":{}}},{\"id\":\"3\",\"type\":\"terms\",\"schema\":\"group\",\"params\":{\"field\":\"service_name.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\"}}],\"listeners\":{}}",
+          "uiStateJSON": "{}",
+          "description": "",
+          "version": 1,
+          "kibanaSavedObjectMeta": {
+            "searchSourceJSON": "{\"index\":\"dede_job_data\",\"query\":{\"query_string\":{\"analyze_wildcard\":true,\"query\":\"*\"}},\"filter\":[]}"
+          }
+          }
+
+
+f1_by_service_name =  {
+          "title": "f1_by_service_name",
+          "visState": "{\"title\":\"accp_by_service_name\",\"type\":\"line\",\"params\":{\"addLegend\":true,\"addTimeMarker\":false,\"addTooltip\":true,\"defaultYExtents\":false,\"drawLinesBetweenPoints\":true,\"interpolate\":\"linear\",\"radiusRatio\":9,\"scale\":\"linear\",\"setYExtents\":false,\"shareYAxis\":true,\"showCircles\":true,\"smoothLines\":false,\"times\":[],\"yAxis\":{}},\"aggs\":[{\"id\":\"1\",\"type\":\"max\",\"schema\":\"metric\",\"params\":{\"field\":\"f1\"}},{\"id\":\"2\",\"type\":\"histogram\",\"schema\":\"segment\",\"params\":{\"field\":\"iteration\",\"interval\":200,\"extended_bounds\":{}}},{\"id\":\"3\",\"type\":\"terms\",\"schema\":\"group\",\"params\":{\"field\":\"service_name.raw\",\"size\":0,\"order\":\"desc\",\"orderBy\":\"1\"}}],\"listeners\":{}}",
+          "uiStateJSON": "{}",
+          "description": "",
+          "version": 1,
+          "kibanaSavedObjectMeta": {
+            "searchSourceJSON": "{\"index\":\"dede_job_data\",\"query\":{\"query_string\":{\"analyze_wildcard\":true,\"query\":\"*\"}},\"filter\":[]}"
+          }
+          }
+
+#Index visualization documents in the .kibana index
+es.index(index=".kibana", doc_type="visualization",id="services_parameters", body=services_parameters)
+es.index(index=".kibana", doc_type="visualization", id="job_data_leaderboard",body=job_data_leaderboard)
+es.index(index=".kibana", doc_type="visualization", id="accp_by_service_name",body=accp_by_service_name)
+es.index(index=".kibana", doc_type="visualization", id="f1_by_service_name",body=f1_by_service_name)
+
+
+#Prepare dashboard document
+services_dashboard = {"title": "Services Dashboard",
+          "hits": 0,
+          "description": "",
+          "panelsJSON": '[{"col":1,"id":"job_data_leaderboard","panelIndex":3,"row":1,"size_x":6,"size_y":4,"type":"visualization"},{"col":7,"id":"services_parameters","panelIndex":4,"row":1,"size_x":6,"size_y":4,"type":"visualization"},{"id":"accp_by_service_name","type":"visualization","panelIndex":5,"size_x":12,"size_y":5,"col":1,"row":5},{"id":"f1_by_service_name","type":"visualization","panelIndex":6,"size_x":12,"size_y":4,"col":1,"row":10}]',
+          "optionsJSON": '{"darkTheme":false}',
+          "uiStateJSON": '{"P-5":{"spy":{"mode":{"name":null,"fill":false}}}}',
+          "version": 1,
+          "timeRestore": "false",
+          "kibanaSavedObjectMeta": {
+            "searchSourceJSON": '{"filter":[{"query":{"query_string":{"analyze_wildcard":true,"query":"*"}}}]}'
+          }
+          }
+#Index dashboard document in the .kibana index
+es.index(index=".kibana", doc_type="dashboard", id="Services-Dashboard",body=services_dashboard)
